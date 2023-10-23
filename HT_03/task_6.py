61cff857a41e5259da97e6f6434a0c65356f664a
ziriael-absten <innli1322@gmail.com>
2023-10-23 15:20:23 +0300

    New changes in task 6

--- HT_03/task_6.py
+++ HT_03/task_6.py
@@ -8,18 +8,15 @@ lst_lst = []
 dict_lst = []
 tuple_lst = []
 for value in values_dict.values():
-    if isinstance(value, int) or isinstance(value, float):
+    if isinstance(value, (int, float)):
         nums_lst.append(value)
     elif isinstance(value, str):
         str_lst.append(value)
-    elif isinstance(value, list):
-        lst_lst.append(value)
-    elif isinstance(value, tuple):
-        tuple_lst.append(value)
+    elif isinstance(value, (list, tuple)):
+        lst_lst.append(list(value))
     elif isinstance(value, dict):
         dict_lst.append(list(value.items()))
 print(f"The largest value of numbers is {(max(nums_lst))} and the smallest {min(nums_lst)}")
 print(f"The largest value of strings is {(max(str_lst))} and the smallest {min(str_lst)}")
 print(f"The largest value of dictionaries is {max(dict_lst)} and the smallest {min(dict_lst)}")
-print(F"The largest value of tuples is {(max(tuple_lst))} and the smallest {min(tuple_lst)}")
-print(f"The largest value of lists is {(max(lst_lst))} and the smallest {min(lst_lst)}")
+print(f"The largest value of lists and tuples is {(max(lst_lst))} and the smallest {min(lst_lst)}")
