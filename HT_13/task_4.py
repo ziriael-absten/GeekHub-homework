801746d59bf0900ffa584288bdb0b7b20c29c60d
ziriael-absten <innli1322@gmail.com>
2023-12-02 19:34:28 +0200

    Fix task 4 in homework 13

--- HT_13/task_4.py
+++ HT_13/task_4.py
@@ -8,8 +8,15 @@ class CustomList:
 
 
     def __getitem__(self, index):
-        if index < 1:
-            raise IndexError("Index must be greater than or equal to 1")
+        if isinstance(index, slice):
+            start = index.start - 1 if index.start is not None else None
+            stop = index.stop - 1 if index.stop is not None else None
+            step = index.step
+            return self.elements[start:stop:step]
+        elif -len(self.elements) <= index < 0:
+            return self.elements[index]
+        elif index == 0:
+            raise IndexError("Index must be greater or smaller than 0")
         try:
             return self.elements[index - 1]
         except IndexError:
@@ -17,12 +24,18 @@ class CustomList:
 
 
     def __setitem__(self, index, value):
-        if index < 1:
-            raise IndexError("Index must be greater than or equal to 1")
+        if -len(self.elements) <= index < 0:
+            self.elements[index] = value
+        elif index == 0:
+            raise IndexError("Index must be greater or smaller than 0")
         try:
             self.elements[index - 1] = value
         except IndexError:
             raise IndexError("Index out of range")
+
+
+    def __iter__(self):
+        return iter(self.elements)
 
 
     def __str__(self):
@@ -30,7 +43,11 @@ class CustomList:
 
 
 custom_list = CustomList(10, 20, 30, 40, 50)
-print(custom_list[1])
-print(custom_list[3])
-custom_list[2] = 25
+print(custom_list[-1])
+print(custom_list[-3])
+print(custom_list[-3:])
+custom_list[-2] = 35
 print(custom_list)
+for element in custom_list:
+    print(element)
+    
