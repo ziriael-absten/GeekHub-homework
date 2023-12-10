9ba2bbb1afcb4f007d6e4763142bfe4e0372fb2c
ziriael-absten <innli1322@gmail.com>
2023-12-10 20:03:20 +0200

    Add task 2 and task 3 to homework 15

--- HT_15/task_2.py
+++ HT_15/task_2.py
@@ -8,6 +8,7 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_data(url):
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
@@ -51,6 +52,7 @@ def start():
         url = start_url + next_link['href']
         time.sleep(10)
     write_to_csv(result)
+
 
 if __name__ == "__main__":
     start()
---
+++ HT_15/task_3/task3/spiders/google_extension_spider.py
@@ -0,0 +1,45 @@
+# 3. Використовуючи Scrapy, заходите на "https://chrome.google.com/webstore/sitemap", переходите 
+# на кожен лінк з тегів <loc>, з кожного лінка берете посилання на сторінки екстеншенів, парсите 
+# їх і зберігаєте в CSV файл ID, назву та короткий опис кожного екстеншена (пошукайте уважно де 
+# його можна взяти)
+
+import scrapy
+
+
+class GoogleExtensionSpider(scrapy.Spider):
+    name = 'google_extension'
+    start_urls = ['https://chrome.google.com/webstore/sitemap']
+
+
+    def parse(self, response):
+        # Extracting links from <loc> tags in the sitemap
+        ns = {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+        sitemaps = response.xpath('//ns:sitemap', namespaces=ns)
+
+        for sitemap in sitemaps:
+            loc = sitemap.xpath('./ns:loc/text()', namespaces=ns).get()
+            yield scrapy.Request(url=loc, callback=self.parse_extensions_page)
+
+
+    def parse_extensions_page(self, response):
+        ns = {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+        urls = response.xpath('//ns:url', namespaces=ns)
+        for url in urls:
+            loc = url.xpath('./ns:loc/text()', namespaces=ns).get()
+            yield scrapy.Request(url=loc, callback=self.parse_extension_details)
+
+
+    def parse_extension_details(self, response):
+        strings = response.url.split('/')
+        extension_id = strings[-1]
+        extension_name = response.css('meta[property="og:title"]::attr(content)').get()
+        short_description = response.css('meta[property="og:description"]::attr(content)').get()
+        print(f"extension_id: {extension_id}")
+        print(f"extension_name: {extension_name}")
+        print(f"short_description: {short_description}")
+        if extension_id and extension_name and short_description:
+            yield {
+                'Extension ID': extension_id.strip(),
+                'Extension Name': extension_name.strip(),
+                'Short Description': short_description.strip(),
+            }
